e32d74f381a89ccfaf32cebbc4749c9da0670dcd
Qout <168398024+qoutalzyadi@users.noreply.github.com>
2024-05-15 10:45:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("Analysis of Job Advertisements in the Kingdom")
 st.write("Hello, I work as a Recruitment Officer at a volunteer organization dedicated to employing and developing young people in the Kingdom. Recently, I have been tasked with analyzing job advertisements to understand the labor market needs and employment trends for young job seekers.")
 st.write("I started collecting job data from various sources, including online recruitment and local newspapers. I reviewed the data to understand the distribution of jobs by region, sector, and experience requirements.")
 
-st.image(""C:\Users\qouta\Downloads\msg6655694347-23191.000001.jpg ", caption=This is a local image", use_column_width=True)
+st.image("C:\Users\qouta\Downloads\msg6655694347-23191.000001.jpg ", caption=This is a local image", use_column_width=True)
 ")
 
 st.write("Based on the analyzed data, it was found that 403 job advertisements specifically sought women, 480 job advertisements sought men, and 586 job advertisements did not specify a particular gender.")
